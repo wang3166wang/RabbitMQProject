56f214b77756c5293e51b45852c79b8ff4493572
毕加思索 <48183331+wang3166wang@users.noreply.github.com>
2022-09-02 11:57:48 +0800

    1

---
+++ order-service-manager/src/main/java/com/imooc/food/orderservicemanager/config/AsyncConfigurer.java
@@ -0,0 +1,13 @@
+package com.imooc.food.orderservicemanager.config;
+
+/**
+ * @ClassName: AsyncConfigurer
+ * @description:
+ * @date: 2022/9/2 0002 星期五 11:57
+ * @Company: 鑫开拓电子商务有限公司
+ * @author: wsq
+ * @version: V1.0
+ * @Copyright: Copyright (c) 2022
+ */
+public class AsyncConfigurer {
+}
